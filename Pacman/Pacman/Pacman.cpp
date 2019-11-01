b719960cfd2bf9aaf093b50f03b71adbd89e3c00
MaxOsman <57242759+MaxOsman@users.noreply.github.com>
2019-11-01 02:29:41 +0000

    Add files via upload

---
+++ Pacman/Pacman/Pacman.cpp
@@ -0,0 +1,474 @@
+#include "Pacman.h"
+
+#include <sstream>
+
+
+Pacman::Pacman(int argc, char* argv[]) : Game(argc, argv), _cPacmanSpeed(0.2f), _cPacmanFrameTime(250), _cMunchieFrameTime(500)
+{
+	_pacmanDir[0] = 0;
+	_pacmanDir[1] = 0;
+	_pacmanDir[2] = 0;
+	_pacmanDir[3] = 0;
+	_paused = false;
+	_startmenu = true;
+	_helpmenu = false;
+	_pKeyDown = false;
+	_aKeyDown = false;
+
+	//Initialise important Game aspects
+	Graphics::Initialise(argc, argv, this, 1024, 768, false, 25, 25, "Pacman", 60);
+	Input::Initialise();
+
+	// Start the Game Loop - This calls Update and Draw in game loop
+	Graphics::StartGameLoop();
+}
+
+Pacman::~Pacman()
+{
+	// remember, DO CLEAN UP
+
+	delete _pacmanTexture[0];
+	delete _pacmanTexture[1];
+	delete _pacmanTexture[2];
+	delete _pacmanTexture[3];
+	delete _pacmanSourceRect[0];
+	delete _pacmanSourceRect[1];
+	delete _pacmanSourceRect[2];
+	delete _pacmanSourceRect[3];
+	delete _munchieTexture;
+	delete _munchieSourceRect;
+	delete _menuBackground;
+	delete _startBackground;
+	delete _arrowTexture;
+	delete _arrowPos;
+	delete _menuRectangle;
+	delete _arrowSourceRect;
+	delete _menuStringPosition;
+	delete _titleStringPosition;
+	delete _startStringPosition;
+	delete _helpStringPosition;
+	delete _quitStringPosition;
+}
+
+void Pacman::LoadContent()
+{
+	_menuBackground = new Texture2D();
+	_startBackground = new Texture2D();
+	_arrowTexture = new Texture2D();
+
+	//Help menu graphics
+	_c1Texture = new Texture2D();
+	_c2Texture = new Texture2D();
+	_c3Texture = new Texture2D();
+	_c4Texture = new Texture2D();
+
+	_c1Texture->Load("Textures/controls1.tga", false);
+	_c2Texture->Load("Textures/controls2.tga", false);
+	_c3Texture->Load("Textures/controls3.tga", false);
+	_c4Texture->Load("Textures/controls4.tga", false);
+
+	_c1Rect = new Rect(200.0f, 50.0f, 192, 128);
+	_c2Rect = new Rect(Graphics::GetViewportWidth() - 400.0f, 50.0f, 192, 128);
+	_c3Rect = new Rect(200.0f, Graphics::GetViewportHeight() - 400.0f, 192, 128);
+	_c4Rect = new Rect(Graphics::GetViewportWidth() - 400.0f, Graphics::GetViewportHeight() - 400.0f, 192, 128);
+
+	//Start menu assets
+	_arrowPos = new Vector2((Graphics::GetViewportWidth() / 2.0f) + 128, (Graphics::GetViewportHeight() / 2.0f) + 48);
+	_menuBackground->Load("Textures/Transparency.png", false);
+	_startBackground->Load("Textures/Start.png", false);
+	_arrowTexture->Load("Textures/arrow.png", false);
+	_menuRectangle = new Rect(0.0f, 0.0f, Graphics::GetViewportWidth(), Graphics::GetViewportHeight());
+	_arrowSourceRect = new Rect(0.0f, 0.0f, 16, 16);
+	_menuStringPosition = new Vector2(Graphics::GetViewportWidth() / 2.0f, Graphics::GetViewportHeight() / 2.0f);
+	_titleStringPosition = new Vector2((Graphics::GetViewportWidth() / 2.0f)-64, (Graphics::GetViewportHeight() / 2.0f)-128);
+	_startStringPosition = new Vector2((Graphics::GetViewportWidth() / 2.0f)-128, (Graphics::GetViewportHeight() / 2.0f)+64);
+	_helpStringPosition = new Vector2((Graphics::GetViewportWidth() / 2.0f) - 128, (Graphics::GetViewportHeight() / 2.0f) + 128);
+	_quitStringPosition = new Vector2((Graphics::GetViewportWidth() / 2.0f) - 128, (Graphics::GetViewportHeight() / 2.0f) + 192);
+
+	//Text for help screen
+	_p1Pos = new Vector2(260.0f, 200.0f);
+	_p2Pos = new Vector2(Graphics::GetViewportWidth() - 340.0f, 200.0f);
+	_p3Pos = new Vector2(260.0f, Graphics::GetViewportHeight() - 250.0f);
+	_p4Pos = new Vector2(Graphics::GetViewportWidth() - 340.0f, Graphics::GetViewportHeight() - 250.0f);
+	_spacePos = new Vector2((Graphics::GetViewportWidth() / 2) - 100.0f, Graphics::GetViewportHeight() - 50.0f);
+
+	_arrowPlace = 0;
+	_arrowOffset = 0;
+
+	// Load Pacman
+	_pacmanTexture[0] = new Texture2D();
+	_pacmanTexture[0]->Load("Textures/Pacman_Red.tga", false);
+	_pacmanPosition[0] = new Vector2(350.0f, 350.0f);
+	_pacmanSourceRect[0] = new Rect(0.0f, 0.0f, 32, 32);
+
+	_pacmanTexture[1] = new Texture2D();
+	_pacmanTexture[1]->Load("Textures/Pacman_Red.tga", false);
+	_pacmanPosition[1] = new Vector2(Graphics::GetViewportWidth() - 350.0f, 350.0f);
+	_pacmanSourceRect[1] = new Rect(0.0f, 0.0f, 32, 32);
+
+	_pacmanTexture[2] = new Texture2D();
+	_pacmanTexture[2]->Load("Textures/Pacman_Blue.tga", false);
+	_pacmanPosition[2] = new Vector2(350.0f, Graphics::GetViewportHeight() - 350.0f);
+	_pacmanSourceRect[2] = new Rect(0.0f, 0.0f, 32, 32);
+
+	_pacmanTexture[3] = new Texture2D();
+	_pacmanTexture[3]->Load("Textures/Pacman_Blue.tga", false);
+	_pacmanPosition[3] = new Vector2(Graphics::GetViewportWidth() - 350.0f, Graphics::GetViewportHeight() - 350.0f);
+	_pacmanSourceRect[3] = new Rect(0.0f, 0.0f, 32, 32);
+
+	for (int i = 0; i < 4; i++)
+	{
+		_pacmanCurrentFrameTime[i] = 0;
+		_pacmanFrame[i] = 0;
+	}
+	
+	// Load Munchie
+	_munchieTexture = new Texture2D();
+	_munchieTexture->Load("Textures/Munchies.png", true);
+	_munchiePosition = new Vector2(100.0f, 450.0f);
+	_munchieSourceRect = new Rect(0.0f, 0.0f, 12, 12);
+	_munchieCurrentFrameTime = 0;
+	_munchieFrame = 0;
+
+	// Set string position
+	_stringPosition = new Vector2(10.0f, 25.0f);
+}
+
+void Pacman::Update(int elapsedTime)
+{
+	// Gets the current state of the keyboard
+	Input::KeyboardState* keyboardState = Input::Keyboard::GetState();
+
+	if (!_startmenu)
+	{
+		if (!_paused)
+		{
+			//Animate munchie
+			_munchieCurrentFrameTime += elapsedTime;
+			if (_munchieCurrentFrameTime > _cMunchieFrameTime)
+			{
+				_munchieFrame++;
+				if (_munchieFrame >= 2)
+					_munchieFrame = 0;
+
+				_munchieCurrentFrameTime = 0;
+			}
+			_munchieSourceRect->X = _munchieSourceRect->Width * _munchieFrame;
+
+			// Checks if key is pressed, Pac1
+			if (keyboardState->IsKeyDown(Input::Keys::W))
+			{
+				_pacmanPosition[0]->Y -= _cPacmanSpeed * elapsedTime; //Moves Pacman across Y axis
+				_pacmanDir[0] = 3;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::A))
+			{
+				_pacmanPosition[0]->X -= _cPacmanSpeed * elapsedTime; //Moves Pacman across X axis
+				_pacmanDir[0] = 2;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::S))
+			{
+				_pacmanPosition[0]->Y += _cPacmanSpeed * elapsedTime; //Moves Pacman across Y axis
+				_pacmanDir[0] = 1;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::D))
+			{
+				_pacmanPosition[0]->X += _cPacmanSpeed * elapsedTime; //Moves Pacman across X axis
+				_pacmanDir[0] = 0;
+			}
+
+			if (keyboardState->IsKeyDown(Input::Keys::W) || keyboardState->IsKeyDown(Input::Keys::A) || keyboardState->IsKeyDown(Input::Keys::S) || keyboardState->IsKeyDown(Input::Keys::D))
+			{
+				_pacmanCurrentFrameTime[0] += elapsedTime;
+				if (_pacmanCurrentFrameTime[0] > _cPacmanFrameTime)
+				{
+					_pacmanFrame[0]++;
+					if (_pacmanFrame[0] >= 2)
+						_pacmanFrame[0] = 0;
+
+					_pacmanCurrentFrameTime[0] = 0;
+				}
+			}
+
+			// Checks if key is pressed, Pac2
+			if (keyboardState->IsKeyDown(Input::Keys::I))
+			{
+				_pacmanPosition[1]->Y -= _cPacmanSpeed * elapsedTime; 
+				_pacmanDir[1] = 3;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::J))
+			{
+				_pacmanPosition[1]->X -= _cPacmanSpeed * elapsedTime; 
+				_pacmanDir[1] = 2;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::K))
+			{
+				_pacmanPosition[1]->Y += _cPacmanSpeed * elapsedTime; 
+				_pacmanDir[1] = 1;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::L))
+			{
+				_pacmanPosition[1]->X += _cPacmanSpeed * elapsedTime; 
+				_pacmanDir[1] = 0;
+			}
+
+			if (keyboardState->IsKeyDown(Input::Keys::I) || keyboardState->IsKeyDown(Input::Keys::J) || keyboardState->IsKeyDown(Input::Keys::K) || keyboardState->IsKeyDown(Input::Keys::L))
+			{
+				_pacmanCurrentFrameTime[1] += elapsedTime;
+				if (_pacmanCurrentFrameTime[1] > _cPacmanFrameTime)
+				{
+					_pacmanFrame[1]++;
+					if (_pacmanFrame[1] >= 2)
+						_pacmanFrame[1] = 0;
+
+					_pacmanCurrentFrameTime[1] = 0;
+				}
+			}
+
+			// Checks if key is pressed, Pac3
+			if (keyboardState->IsKeyDown(Input::Keys::UP))
+			{
+				_pacmanPosition[2]->Y -= _cPacmanSpeed * elapsedTime;
+				_pacmanDir[2] = 3;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::LEFT))
+			{
+				_pacmanPosition[2]->X -= _cPacmanSpeed * elapsedTime; 
+				_pacmanDir[2] = 2;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::DOWN))
+			{
+				_pacmanPosition[2]->Y += _cPacmanSpeed * elapsedTime;
+				_pacmanDir[2] = 1;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::RIGHT))
+			{
+				_pacmanPosition[2]->X += _cPacmanSpeed * elapsedTime;
+				_pacmanDir[2] = 0;
+			}
+
+			if (keyboardState->IsKeyDown(Input::Keys::UP) || keyboardState->IsKeyDown(Input::Keys::LEFT) || keyboardState->IsKeyDown(Input::Keys::DOWN) || keyboardState->IsKeyDown(Input::Keys::RIGHT))
+			{
+				_pacmanCurrentFrameTime[2] += elapsedTime;
+				if (_pacmanCurrentFrameTime[2] > _cPacmanFrameTime)
+				{
+					_pacmanFrame[2]++;
+					if (_pacmanFrame[2] >= 2)
+						_pacmanFrame[2] = 0;
+
+					_pacmanCurrentFrameTime[2] = 0;
+				}
+			}
+
+			// Checks if key is pressed, Pac4
+			if (keyboardState->IsKeyDown(Input::Keys::NUMPAD5))
+			{
+				_pacmanPosition[3]->Y -= _cPacmanSpeed * elapsedTime;
+				_pacmanDir[3] = 3;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::NUMPAD1))
+			{
+				_pacmanPosition[3]->X -= _cPacmanSpeed * elapsedTime;
+				_pacmanDir[3] = 2;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::NUMPAD2))
+			{
+				_pacmanPosition[3]->Y += _cPacmanSpeed * elapsedTime;
+				_pacmanDir[3] = 1;
+			}
+			else if (keyboardState->IsKeyDown(Input::Keys::NUMPAD3))
+			{
+				_pacmanPosition[3]->X += _cPacmanSpeed * elapsedTime;
+				_pacmanDir[3] = 0;
+			}
+
+			if (keyboardState->IsKeyDown(Input::Keys::NUMPAD5) || keyboardState->IsKeyDown(Input::Keys::NUMPAD1) || keyboardState->IsKeyDown(Input::Keys::NUMPAD2) || keyboardState->IsKeyDown(Input::Keys::NUMPAD3))
+			{
+				_pacmanCurrentFrameTime[3] += elapsedTime;
+				if (_pacmanCurrentFrameTime[3] > _cPacmanFrameTime)
+				{
+					_pacmanFrame[3]++;
+					if (_pacmanFrame[3] >= 2)
+						_pacmanFrame[3] = 0;
+
+					_pacmanCurrentFrameTime[3] = 0;
+				}
+			}
+
+			// Stuff for all 4
+			for(int i = 0; i < 4; i++)
+			{
+				// Screen loop
+				if (_pacmanPosition[i]->X > Graphics::GetViewportWidth())
+				{
+					_pacmanPosition[i]->X = -static_cast<float>(_pacmanSourceRect[i]->Width);	//One is float, other is int
+				}
+				if (_pacmanPosition[i]->X < -_pacmanSourceRect[i]->Width)
+				{
+					_pacmanPosition[i]->X = static_cast<float>(Graphics::GetViewportWidth());
+				}
+				if (_pacmanPosition[i]->Y > Graphics::GetViewportHeight())
+				{
+					_pacmanPosition[i]->Y = -static_cast<float>(_pacmanSourceRect[i]->Height);
+				}
+				if (_pacmanPosition[i]->Y < -_pacmanSourceRect[i]->Height)
+				{
+					_pacmanPosition[i]->Y = static_cast<float>(Graphics::GetViewportHeight());
+				}
+
+				//Change direction, animate
+				_pacmanSourceRect[i]->Y = _pacmanSourceRect[i]->Height * static_cast<float>(_pacmanDir[i]);
+				_pacmanSourceRect[i]->X = _pacmanSourceRect[i]->Width * _pacmanFrame[i];
+			}
+		}
+
+		if (keyboardState->IsKeyDown(Input::Keys::P) && !_pKeyDown)
+		{
+			_pKeyDown = true;
+			_paused = !_paused;
+		}
+		if (keyboardState->IsKeyUp(Input::Keys::P))
+			_pKeyDown = false;
+	}
+	else if(!_helpmenu)
+	{
+		if (keyboardState->IsKeyDown(Input::Keys::S) && !_aKeyDown)
+		{
+			_arrowPos->Y = (Graphics::GetViewportHeight() / 2.0f) + 48;
+			if (_arrowPlace >= 2)
+			{
+				_arrowPlace = 0;
+			}
+			else
+			{
+				_arrowPlace++;
+			}
+			_aKeyDown = true;
+			_arrowOffset = 64 * _arrowPlace;
+		}
+
+		if (keyboardState->IsKeyDown(Input::Keys::W) && !_aKeyDown)
+		{
+			_arrowPos->Y = (Graphics::GetViewportHeight() / 2.0f) + 48;
+			if (_arrowPlace <= 0)
+			{
+				_arrowPlace = 2;
+			}
+			else
+			{
+				_arrowPlace--;
+			}
+			_aKeyDown = true;
+			_arrowOffset = 64 * _arrowPlace;
+		}
+
+		if (keyboardState->IsKeyUp(Input::Keys::W) && keyboardState->IsKeyUp(Input::Keys::S))
+			_aKeyDown = false;
+		if (keyboardState->IsKeyDown(Input::Keys::RETURN))
+		{
+			if (_arrowPlace == 0)
+				_startmenu = false;
+			else if (_arrowPlace == 1)
+				_helpmenu = true;
+			if (_arrowPlace == 2)
+			{
+				
+			}
+				//help
+		}
+	}
+	else if (_helpmenu)
+	{
+		if (keyboardState->IsKeyDown(Input::Keys::SPACE))
+		{
+			_helpmenu = false;
+		}
+	}
+}
+
+void Pacman::Draw(int elapsedTime)
+{
+	// Allows us to easily create a string
+	std::stringstream stream;
+	stream << "Pacman X: " << _pacmanPosition[0]->X << " Y: " << _pacmanPosition[0]->Y;
+
+	SpriteBatch::BeginDraw(); // Starts Drawing
+	for (int i = 0; i < 4; i++)
+	{
+		SpriteBatch::Draw(_pacmanTexture[i], _pacmanPosition[i], _pacmanSourceRect[i]); // Draws Pacman
+	}
+	
+	// Draw Munchie
+	SpriteBatch::Draw(_munchieTexture, _munchiePosition, _munchieSourceRect); // Draws Pacman
+	
+	// Draws String
+	SpriteBatch::DrawString(stream.str().c_str(), _stringPosition, Color::Green);
+
+	if (_paused)
+	{
+		std::stringstream menuStream; 
+		menuStream << "PAUSED";
+		SpriteBatch::Draw(_menuBackground, _menuRectangle, nullptr); 
+		SpriteBatch::DrawString(menuStream.str().c_str(), _menuStringPosition, Color::Red);
+	}
+
+	if (_startmenu)
+	{
+		SpriteBatch::Draw(_startBackground, _menuRectangle, nullptr);
+
+		_arrowPos->Y += _arrowOffset;
+		_arrowOffset = 0;
+
+		if (!_helpmenu)
+		{
+			SpriteBatch::Draw(_arrowTexture, _arrowPos, _arrowSourceRect);
+
+			std::stringstream titleStream;
+			titleStream << "PACMAN";
+			SpriteBatch::DrawString(titleStream.str().c_str(), _titleStringPosition, Color::Red);
+
+			std::stringstream startStream;
+			startStream << "Start Game";
+			SpriteBatch::DrawString(startStream.str().c_str(), _startStringPosition, Color::Red);
+
+			std::stringstream helpStream;
+			helpStream << "How To Play";
+			SpriteBatch::DrawString(helpStream.str().c_str(), _helpStringPosition, Color::Red);
+
+			std::stringstream quitStream;
+			quitStream << "Quit";
+			SpriteBatch::DrawString(quitStream.str().c_str(), _quitStringPosition, Color::Red);
+		}
+	}
+
+	// Help menu
+	if (_helpmenu)
+	{
+		SpriteBatch::Draw(_c1Texture, _c1Rect, nullptr);
+		SpriteBatch::Draw(_c2Texture, _c2Rect, nullptr);
+		SpriteBatch::Draw(_c3Texture, _c3Rect, nullptr);
+		SpriteBatch::Draw(_c4Texture, _c4Rect, nullptr);
+
+		std::stringstream p1;
+		p1 << "Player 1";
+		SpriteBatch::DrawString(p1.str().c_str(), _p1Pos, Color::Red);
+
+		std::stringstream p2;
+		p2 << "Player 2";
+		SpriteBatch::DrawString(p2.str().c_str(), _p2Pos, Color::Red);
+
+		std::stringstream p3;
+		p3 << "Player 3";
+		SpriteBatch::DrawString(p3.str().c_str(), _p3Pos, Color::Cyan);
+
+		std::stringstream p4;
+		p4 << "Player 4";
+		SpriteBatch::DrawString(p4.str().c_str(), _p4Pos, Color::Cyan);
+
+		std::stringstream space;
+		space << "Press SPACE to go back";
+		SpriteBatch::DrawString(space.str().c_str(), _spacePos, Color::Red);
+	}
+
+	SpriteBatch::EndDraw(); // Ends Drawing
+}
---
+++ Pacman/Pacman/Pacman.h
@@ -0,0 +1,99 @@
+#pragma once
+
+// If Windows and not in Debug, this will run without a console window
+// You can use this to output information when debugging using cout or cerr
+#ifdef WIN32 
+	#ifndef _DEBUG
+		#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+	#endif
+#endif
+
+// Just need to include main header file
+#include "S2D/S2D.h"
+
+// Reduces the amount of typing by including all classes in S2D namespace
+using namespace S2D;
+
+// Declares the Pacman class which inherits from the Game class.
+// This allows us to overload the Game class methods to help us
+// load content, draw and update our game.
+class Pacman : public Game
+{
+private:
+	// Data to represent Pacman
+	Vector2* _pacmanPosition[4];
+	Rect* _pacmanSourceRect[4];
+	Texture2D* _pacmanTexture[4];
+	Vector2* _arrowPos;
+	Rect* _arrowSourceRect;
+	Texture2D* _arrowTexture;
+
+	//Pacman data
+	int _pacmanDir[4];	//0 = right, 1 = down, 2 = left, 3 = up
+	const float _cPacmanSpeed;
+	int _pacmanFrame[4];
+	int _pacmanCurrentFrameTime[4];
+	const int _cPacmanFrameTime;
+
+	//Munchie data
+	const int _cMunchieFrameTime;
+	int _munchieCurrentFrameTime;
+	int _munchieFrame;
+
+	Vector2* _munchiePosition;
+	Rect* _munchieSourceRect;
+	Texture2D* _munchieTexture;
+
+	//Menu data
+	bool _paused;
+	bool _startmenu;
+	bool _helpmenu;
+	Texture2D* _menuBackground;
+	Texture2D* _startBackground;
+
+	Texture2D* _c1Texture;
+	Rect* _c1Rect;
+	Texture2D* _c2Texture;
+	Rect* _c2Rect;
+	Texture2D* _c3Texture;
+	Rect* _c3Rect;
+	Texture2D* _c4Texture;
+	Rect* _c4Rect;
+
+	Rect* _menuRectangle;
+	Vector2* _menuStringPosition;
+	Vector2* _titleStringPosition;
+	Vector2* _startStringPosition;
+	Vector2* _helpStringPosition;
+	Vector2* _quitStringPosition;
+
+	Vector2* _p1Pos;
+	Vector2* _p2Pos;
+	Vector2* _p3Pos;
+	Vector2* _p4Pos;
+	Vector2* _spacePos;
+
+	bool _pKeyDown;
+	bool _aKeyDown;
+	signed int _arrowPlace;	//0 = start, 1 = help, 2 = quit
+	int _arrowOffset;
+	
+	// Position for String
+	Vector2* _stringPosition;
+
+public:
+	/// <summary> Constructs the Pacman class. </summary>
+	Pacman(int argc, char* argv[]);
+
+	/// <summary> Destroys any data associated with Pacman class. </summary>
+	virtual ~Pacman();
+
+	/// <summary> All content should be loaded in this method. </summary>
+	void virtual LoadContent();
+
+	/// <summary> Called every frame - update game logic here. </summary>
+	void virtual Update(int elapsedTime);
+
+	/// <summary> Called every frame - draw game here. </summary>
+	void virtual Draw(int elapsedTime);
+};
